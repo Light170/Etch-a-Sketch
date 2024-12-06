0394988b781992ebb9f5fa0b7b3dff8822ef7b87
RaitoY <yonatanssj@gmail.com>
2024-12-06 04:07:28 +0200

    Commit solution

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Etch-a-Sketch
+In this project we are creating a Etch-a-Sketch, pixel like one,
+by creating a 16x16 grid of square divs.
+
+There is an option to change the size of the grid.
--- js-file.js
+++ js-file.js
@@ -4,4 +4,39 @@ for (let i = 0; i < 16 * 16; i++) {
     const square = document.createElement('div');
     square.classList.add('grid-square');
     container.appendChild(square);
+
+    square.addEventListener('mouseenter', function() {
+        square.classList.add('hovered');
+    });
+}
+
+const changeGridBtn = document.getElementById("change-grid-btn");
+
+changeGridBtn.addEventListener('click', function() {
+    let squaresPerSide =  parseInt(prompt("Enter the number of squares per side:"));
+    if (squaresPerSide > 100) {
+        alert("Maximum squares per side is 100.");
+        squaresPerSide = 100;
+    }
+    else if (squaresPerSide < 1) {
+        alert("Please enter a positive number.");
+        return;
+    }
+    createGrid(squaresPerSide);
+});
+
+function createGrid(squaresPerSide) {
+    container.replaceChildren();
+
+    for (let i = 0; i < squaresPerSide * squaresPerSide; i++) {
+        const square = document.createElement('div');
+        square.classList.add('grid-square');
+        square.style.width = `calc(100% / ${squaresPerSide})`;
+        square.style.height = `calc(100% / ${squaresPerSide})`;
+        container.appendChild(square);
+
+        square.addEventListener('mouseenter', function() {
+            square.classList.add('hovered');
+        });
+    }
 }
